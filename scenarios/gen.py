26f472dd92ef65261e3945220ee342721834af4e
Marc Pujol-Gonzalez <mpujol@iiia.csic.es>
2013-07-16 06:12:35 +0200

    Minor improvements to the random scenario generator

--- scenarios/gen.py
+++ scenarios/gen.py
@@ -5,17 +5,39 @@ import random
 
 maps = {
     'paris': [
-            52232, 44376, 48293, 64614, 19357, 32134, 45104, 52352,
-            35932, 51370, 27415, 63803, 52107, 44900, 50962, 20820,
-            53053, 24887, 61658, 49476, 36306, 53189, 29928, 28444,
-            47800, 64915, 24660, 23963, 63861, 41726, 63873, 39083,
-            64546, 38547, 64256, 55238, 47041
+        52232, 44376, 48293, 64614, 19357, 32134, 45104, 52352,
+        35932, 51370, 27415, 63803, 52107, 44900, 50962, 20820,
+        53053, 24887, 61658, 49476, 36306, 53189, 29928, 28444,
+        47800, 64915, 24660, 23963, 63861, 41726, 63873, 39083,
+        64546, 38547, 64256, 55238, 47041
+    ],
+    'kobe': [
+        18733, 25421, 36367, 11442, 10110, 33848, 23545, 17935,
+        15182, 18409, 24151, 23165, 27008, 16146, 16627, 15630,
+        16297, 33319, 22697, 20245, 11302, 14860, 9820, 19127,
+        10305, 14571, 28899
     ],
 }
 
+paths = {
+    'paris': '../../roborescue/maps/gml/paris/map.gml',
+    'kobe' : '../../roborescue/maps/gml/Kobe2013/map/map.gml',
+}
+
+def buildings(mapname):
+    def fetch_building(line):
+        return int(line.split('"')[1])
+
+    fh = open(paths[mapname], 'r')
+    buildings = [fetch_building(line) for line in fh if 'building gml:id="' in line]
+    fh.close()
+    return buildings
+
+
 def main(args):
     mapname, fires, agents = args[0], int(args[1]), int(args[2])
-    points = maps[mapname]
+    #points = maps[mapname]       # Use manually selected points
+    points = buildings(mapname)   # Use random points from the map
 
     print '<?xml version="1.0" encoding="UTF-8"?>'
     print ''
